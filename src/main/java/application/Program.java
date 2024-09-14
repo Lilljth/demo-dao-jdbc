def6d4b77a11fb3ee7ed3ccb7bd4a7ee3938903f
Allisia Franca <allisialive@gmail.com>
2024-09-14 14:36:34 -0300

    SellerDaoJDBC and DaoFactory

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,5 +13,7 @@ public class Program {
         Department d = new Department(1, "books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, d);
         System.out.println(seller);
+
+        SellerDao sellerDao = new DaoFactory().createSellerDao();
     }
 }
