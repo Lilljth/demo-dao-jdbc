56c8a084aabc3ca61c506b05da291ecbc841304e
Allisia Franca <allisialive@gmail.com>
2024-09-14 14:07:57 -0300

    Department entity class

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -3,11 +3,12 @@ package application;
 import java.sql.Connection;
 
 import db.DB;
+import model.entities.Department;
 
 public class Program {
     public static void main(String[] args) {
 
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Department d = new Department(1, "books");
+        System.out.println(d);
     }
 }
