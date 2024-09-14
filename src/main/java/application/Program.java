352d41eee422acb4ba0ed3a4656cc7c19e583cfe
Allisia Franca <allisialive@gmail.com>
2024-09-14 14:17:29 -0300

    Seller entity class

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,14 +1,15 @@
 package application;
 
-import java.sql.Connection;
+import java.util.Date;
 
-import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
 
         Department d = new Department(1, "books");
-        System.out.println(d);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, d);
+        System.out.println(seller);
     }
 }
